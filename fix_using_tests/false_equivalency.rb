b62fb4bc9e97feb4aab26106f8adc91b77c4d5f4
malcolm staso <mstaso1@gmail.com>
2020-05-06 16:18:43 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -6,6 +6,12 @@ def get_user_input
   gets.chomp
 end
 
+def prompt_user
+  puts "What would you like to do?"
+  puts "1.) Eat a hamburger."
+  puts "2.) Eat a ham."
+end
+
 
 def selection(get_user_input)
   if get_user_input = 1
