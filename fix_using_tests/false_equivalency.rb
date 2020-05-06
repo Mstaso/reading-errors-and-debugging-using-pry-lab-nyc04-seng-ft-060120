2014c6a6b1a2ad68c1e9fb9090aa58471850f7e8
malcolm staso <mstaso1@gmail.com>
2020-05-06 16:18:18 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -7,10 +7,10 @@ def get_user_input
 end
 
 
-def selection(num)
-  if num = 1
+def selection(get_user_input)
+  if get_user_input = 1
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif get_user_input = 2
      "HAM HAM HAM IN MY TUMMY"
   end
 end
